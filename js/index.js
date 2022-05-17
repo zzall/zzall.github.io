772d61a8463976ee193b8b54ed67a22b09451d07
张志阵 <1809642355@qq.com>
2022-05-17 12:53:03 +0800

    add validate refre

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
  * @Description: File Description
  * @FilePath: /zzailianlian.github.io/zzailianlian.github.io/js/index.js
  * @LastEditors: zzz
- * @LastEditTime: 2022-05-17 12:46:02
+ * @LastEditTime: 2022-05-17 12:52:35
  */
-if (window.location.href.includes('meitun')) {
+if (window.location.href.includes('refre=meitun')) {
   window.location.href = window.location.href.includes('code')
     ? window.location.href.replace(/code=\w+(?=(&|$))/, 'code=12323')
     : window.location.href + '&code=123234'
