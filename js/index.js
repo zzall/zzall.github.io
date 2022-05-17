fe090f52ec7d936a48c1b2f75bbd4804de13b2f8
张志阵 <1809642355@qq.com>
2022-05-17 13:05:36 +0800

    feat:change url

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
  * @Description: File Description
  * @FilePath: /zzailianlian.github.io/zzailianlian.github.io/js/index.js
  * @LastEditors: zzz
- * @LastEditTime: 2022-05-17 12:52:35
+ * @LastEditTime: 2022-05-17 13:05:16
  */
 if (window.location.href.includes('refre=meitun')) {
-  window.location.href = window.location.href.includes('code')
-    ? window.location.href.replace(/code=\w+(?=(&|$))/, 'code=12323')
-    : window.location.href + '&code=123234'
+  const baseUrl = 'http://sitm.meitun.com:8080/plus/buy?&mtoapp=0&hideNav=1&load_url=true&immerse=1&activeTabIndex=0&isOnlineDebug=true'
+  window.location.href = baseUrl.includes('code')
+    ? baseUrl.replace(/code=\w+(?=(&|$))/, 'code=12323')
+    : baseUrl + '&code=123234'
 }
